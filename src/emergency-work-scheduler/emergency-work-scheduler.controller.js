6cae1c304b607072b33f306e4e5c370f86337722
L2HYUNN <donghyuni98@gmail.com>
2024-11-20 17:30:08 +0900

    refactor: 비상 근무표에 필요한 정보를 등록하는 기능을 작은 함수로 분리

--- src/emergency-work-scheduler/emergency-work-scheduler.controller.js
+++ src/emergency-work-scheduler/emergency-work-scheduler.controller.js
@@ -10,21 +10,31 @@ class EmergencyWorkSchedulerController {
     this.#emergencyWorkSchedulerView = view;
   }
 
-  async generateScheduler() {
+  async #registerEmergencyWorkDate() {
     await safeInput(this.#emergencyWorkSchedulerView.readEmergencyWorkDate, {
       onInput: this.#emergencyWorkSchedulerModel.setEmergencyWorkDate,
       onError: this.#emergencyWorkSchedulerView.printErrorMessage,
     });
+  }
 
+  async #registerWeekdayEmergencyWorkOrder() {
     await safeInput(this.#emergencyWorkSchedulerView.readWeekdayEmergencyWorkOrder, {
       onInput: this.#emergencyWorkSchedulerModel.setWeekdayEmergencyWorkOrder,
       onError: this.#emergencyWorkSchedulerView.printErrorMessage,
     });
+  }
 
+  async #registerWeekendEmergencyWorkOrder() {
     await safeInput(this.#emergencyWorkSchedulerView.readWeekendEmergencyWorkOrder, {
       onInput: this.#emergencyWorkSchedulerModel.setWeekendEmergencyWorkOrder,
       onError: this.#emergencyWorkSchedulerView.printErrorMessage,
     });
+  }
+
+  async generateScheduler() {
+    await this.#registerEmergencyWorkDate();
+    await this.#registerWeekdayEmergencyWorkOrder();
+    await this.#registerWeekendEmergencyWorkOrder();
 
     const workScheduler = this.#emergencyWorkSchedulerModel.createEmergencyWorkSchedule();
 
